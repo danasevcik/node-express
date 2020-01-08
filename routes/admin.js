39bb4fa41693f8b0ae25645d7cb000f69ba41db1
Dana Sevcik <danasevcik@hotmail.com>
2020-01-08 16:46:29 -0500

    adds html views to project

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 router.get('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Submit</button></form>')
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 })
 
 router.post('/product', (req, res, next) => {
