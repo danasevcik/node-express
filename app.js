cb26f775e7984a1f27ca2f8cc60e741c9693b2e8
Dana Sevcik <danasevcik@hotmail.com>
2020-01-27 17:26:26 -0500

    adds pug, handlebars and ejs

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminRoutes = require('./routes/admin.js')
+const adminData = require('./routes/admin.js')
 const shopRoutes = require('./routes/shop.js')
 const path = require('path')
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 //staticly serve the public folder within the current dir
-app.use(adminRoutes);
+app.use(adminData.routes);
 app.use(shopRoutes);
+
 
 app.use('/', (req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'));
